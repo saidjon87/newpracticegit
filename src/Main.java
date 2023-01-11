5cde05f255ac1bb040c72ac582977e414c678dbd
saidjon87 <abdulloev.s@gmail.com>
2023-01-10 21:12:17 -0500

    Third

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world BabaJun!");
         System.out.println("Hello world Mamajun!");
+        System.out.println("Hello world Bratjun!");
     }
 }
